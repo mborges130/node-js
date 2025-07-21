d09e218c2980847ce60f0b833d4f00b8a80e77c0
Mateus Borges <mateusdantas73@icloud.com>
2025-07-21 15:05:04 -0300

    implementa funções para leitura de arquivo e contagem de palavras

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+const fs = require('fs');
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 console.log(link);
+
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    quebraEmParagrafos(texto);
+    verificaPalavrasDuplicadas(texto);
+});
+
+//criar um array de palavras
+// contar as ocorrências de cada palavra
+// montar um obejto com o resultado
+
+//{
+//    "web": 5,
+//    "computador": 4
+//}
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo)=> {
+        return verificaPalavrasDuplicadas(paragrafo);
+    });
+    console.log(contagem);
+    
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    });
+    return resultado;
+}
+
+
